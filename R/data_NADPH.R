d6aef052b3e7421cd5b7777d33cd20b9e705e7a3
Lingyun <nkdailingyun@gmail.com>
2017-05-01 17:07:42 +0800

    include two ITDR datasets for demo and the documentation is written ready for 0.2.0 release

---
+++ R/data_NADPH.R
@@ -0,0 +1,21 @@
+#' ITDR data of NADP/NADPH on K562 lysate.
+#'
+#' A dataset containing the NADP/NADPH ITDR CETSA data on K562 cell lysate.
+#' Search on Proteome Discover 2.0, Cleaned, Scaled, and Fitted.
+#'
+#' @docType data
+#'
+#' @usage attach(data_NADPH)
+#' @usage load(data_NADPH)
+#'
+#' @keywords datasets
+#'
+#' @format A data frame with 24766 rows and 10 doses:
+#' \describe{
+#'   \item{id}{Uniprot ID}
+#'   \item{description}{Protein annotation}
+#'   \item{condition}{Experimental treatment condition plus replicate number}
+#'   ...
+#' }
+#' @source Nayana Prabhu/Par Nordlund
+"data_NADPH"
